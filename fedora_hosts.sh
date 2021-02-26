607d99b73dab4f32f77570f21004862e3f8f810f
John Fereira <jaf30@cornell.edu>
2021-02-26 09:02:51 -0500

    initial commit

---
+++ fedora_hosts.sh
@@ -0,0 +1,9 @@
+export DEV=http://localhost:8984/rest
+export INT=http://aws-110-007.internal.library.cornell.edu:8080/fedora/rest
+export STG=http://aws-110-026.internal.library.cornell.edu:8080/fedora/rest
+export PROD=http://aws-110-124.internal.library.cornell.edu:8080/fedora/rest
+
+export NEWINT=http://aws-108-191.internal.library.cornell.edu:8080/fedora/rest
+export NEWSTG=http://aws-108-233.internal.library.cornell.edu:8080/fedora/rest
+export NEWPROD=
+
